59c37693e59c2471350a7eb670d5f209d058c466
Crowdbotics <team@crowdbotics.com>
2021-05-28 13:59:07 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/holy_forest_dev_22593/MainActivity.java
@@ -0,0 +1,15 @@
+package com.holy_forest_dev_22593;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "holy_forest_dev_22593";
+  }
+}
